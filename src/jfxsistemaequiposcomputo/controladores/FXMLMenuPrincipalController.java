73a7eac8120b94f45a955a8a1fb8604d87a2fcab
Angel Garcia <adjcg15@gmail.com>
2023-05-08 13:01:19 -0600

    Creación del repositorio

---
+++ src/jfxsistemaequiposcomputo/controladores/FXMLMenuPrincipalController.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/javafx/FXML2.java to edit this template
+ */
+package jfxsistemaequiposcomputo.controladores;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+
+/**
+ *
+ * @author ANGEL
+ */
+public class FXMLMenuPrincipalController implements Initializable {
+    @FXML
+    private Label label;
+    
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    
+}
