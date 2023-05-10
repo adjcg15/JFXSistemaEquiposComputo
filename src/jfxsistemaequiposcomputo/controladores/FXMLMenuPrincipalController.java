d69c8385311885ed3a7eed02619a80e735f46a9d
Angel Garcia <adjcg15@gmail.com>
2023-05-10 00:36:42 -0600

    Funciones de redirección en menú principal creadas

--- src/jfxsistemaequiposcomputo/controladores/FXMLMenuPrincipalController.java
+++ src/jfxsistemaequiposcomputo/controladores/FXMLMenuPrincipalController.java
@@ -12,6 +12,10 @@ import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.Pane;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+import jfxsistemaequiposcomputo.pojo.Usuario;
+import jfxsistemaequiposcomputo.utils.Utilidades;
 
 /**
  *
@@ -20,26 +24,52 @@ import javafx.scene.layout.Pane;
 public class FXMLMenuPrincipalController implements Initializable {
     @FXML
     private Pane paneModuloPrivilegiado;
+    private Usuario usuario;
     
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
-    }    
+    }
+    
+    private void cargarOpcionesUsuario() {
+        
+    }
 
     @FXML
     private void clicCerrarSesion(MouseEvent event) {
+        
     }
 
     @FXML
     private void clicRedirigirSolicitarMantenimiento(ActionEvent event) {
+        Stage escenarioSolicitudMantenimiento = new Stage();
+        escenarioSolicitudMantenimiento.setScene(
+            Utilidades.inicializarEscena("vistas/FXMLRegistrarEquipo.fxml")
+        );
+        escenarioSolicitudMantenimiento.setTitle("Solicitar mantenimiento");
+        escenarioSolicitudMantenimiento.initModality(Modality.APPLICATION_MODAL);
+        escenarioSolicitudMantenimiento.showAndWait();
     }
 
     @FXML
     private void clicRedirigirGenerarDiagnostico(ActionEvent event) {
+        Stage escenarioDiagnostico = new Stage();
+        escenarioDiagnostico.setScene(
+            Utilidades.inicializarEscena("vistas/FXMLGenerarDiagnostico.fxml")
+        );
+        escenarioDiagnostico.setTitle("Generar diagnóstico");
+        escenarioDiagnostico.initModality(Modality.APPLICATION_MODAL);
+        escenarioDiagnostico.showAndWait();
     }
 
     @FXML
     private void clicRedirigirGestionarMantenimiento(ActionEvent event) {
+        Stage escenarioMantenimiento = new Stage();
+        escenarioMantenimiento.setScene(
+            Utilidades.inicializarEscena("vistas/FXMLAdministrarMantenimiento.fxml")
+        );
+        escenarioMantenimiento.setTitle("Administrar mantenimiento");
+        escenarioMantenimiento.initModality(Modality.APPLICATION_MODAL);
+        escenarioMantenimiento.showAndWait();
     }
     
 }
