49355061e718ce051cb281201596df7e4ab5c168
Donaji Navarro <ada_hack49@hackademy.mx>
2023-05-10 21:28:00 -0600

    agregar foto, cancelar y regresar en Registrar Equipo

--- src/jfxsistemaequiposcomputo/controladores/RegistrarEquipoController.java
+++ src/jfxsistemaequiposcomputo/controladores/RegistrarEquipoController.java
@@ -4,15 +4,35 @@
  */
 package jfxsistemaequiposcomputo.controladores;
 
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.embed.swing.SwingFXUtils;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Alert;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.RadioButton;
 import javafx.scene.control.TextField;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.control.Toggle;
+import javafx.scene.control.ToggleGroup;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
+import javafx.stage.FileChooser;
+import javafx.stage.Stage;
+import javax.imageio.ImageIO;
+import jfxsistemaequiposcomputo.pojo.SolicitudMantenimiento;
+import jfxsistemaequiposcomputo.utils.Constantes;
+import jfxsistemaequiposcomputo.utils.Utilidades;
 
 /**
  * FXML Controller class
@@ -56,6 +76,29 @@ public class RegistrarEquipoController implements Initializable {
 
     @FXML
     private void clicBtnSeleccionarImagen(ActionEvent event) {
+        FileChooser seleccionImagen = new FileChooser();
+        seleccionImagen.setTitle("Selecciona una imagen");
+        FileChooser.ExtensionFilter filtroDialogo = 
+                new FileChooser.ExtensionFilter("Archivos PNG (*.png)", "*.PNG");
+        seleccionImagen.getExtensionFilters().add(filtroDialogo);
+        Stage escenarioPrincipal = (Stage) tfDescripcionProblema.getScene().getWindow();
+        File archivoSeleccionado = seleccionImagen.showOpenDialog(escenarioPrincipal);
+        visualizarImagen(archivoSeleccionado);
+    }
+    
+    private void visualizarImagen(File imgSeleccionada){
+        if(imgSeleccionada != null){
+            try {
+                BufferedImage bufferImg = ImageIO.read(imgSeleccionada);
+                Image imagenDecodificada = SwingFXUtils.toFXImage(bufferImg, null);
+                ivImagenEquipo.setImage(imagenDecodificada);
+                
+           } catch (IOException ex) {
+                Utilidades.mostrarDialogoSimple("Error", "No fue "
+                        + "posible cargar la imagen, intente más tarde", Alert.AlertType.ERROR);
+            }
+            
+        }
     }
 
     @FXML
@@ -64,10 +107,14 @@ public class RegistrarEquipoController implements Initializable {
 
     @FXML
     private void clicBtnCancelar(ActionEvent event) {
+        Stage escenarioPrincipal = (Stage) tfDescripcionProblema.getScene().getWindow();
+        escenarioPrincipal.close();
     }
 
     @FXML
     private void clicBtnRegresar(MouseEvent event) {
+        Stage escenarioPrincipal = (Stage) tfDescripcionProblema.getScene().getWindow();
+        escenarioPrincipal.close();
     }
     
 }
